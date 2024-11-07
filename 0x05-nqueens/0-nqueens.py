f818a4bef79649b0a71a360109bdb324b6608056
khalidhub7 <louguikhalid7@gmail.com>
2024-11-07 11:02:21 -0800

    up

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,56 +1,44 @@
 #!/usr/bin/python3
-"""N Queens Challenge"""
-
+""" N queens """
 import sys
 
 
-def is_safe(row, col, queens):
-    """Check if placing a queen at position (row, col) is safe"""
-    for r, c in queens:
-        if c == col or abs(r - row) == abs(c - col):
-            return False
-    return True
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-def solve_n_queens(n):
-    """Solve the N Queens problem"""
-    solutions = []
-
-    def backtrack(row, queens):
-        """Use backtracking to find all solutions"""
-        if row == n:
-            solutions.append(queens[:])
-            return
-        for col in range(n):
-            if is_safe(row, col, queens):
-                queens.append([row, col])
-                backtrack(row + 1, queens)
-                queens.pop()
-
-    backtrack(0, [])
-    return solutions
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-def print_solutions(solutions):
-    """Print all solutions"""
-    for queens in solutions:
-        print(queens)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
-
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-
-    solutions = solve_n_queens(n)
-    print_solutions(solutions)
+solve(n)
