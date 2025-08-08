79dfbdf85cb807189ab2b931283d82afdfec957e
khalidhub7 <louguikhalid7@gmail.com>
2025-08-08 12:05:21 +0100

    fix index out of range

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,11 +13,9 @@ def canUnlockAll(boxes):
         node = queue.popleft()
         if node not in visited:
             visited.append(node)
-            for i in boxes[node]:
-                if i not in visited:
-                    queue.append(i)
+            if node < len(boxes):
+                for i in boxes[node]:
+                    if i not in visited:
+                        queue.append(i)
             are_all_unlocked.append(True)
-
-    # print(are_all_unlocked)
-    # print(visited)
     return len(are_all_unlocked) == len(boxes)
