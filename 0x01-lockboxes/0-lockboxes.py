980e1b47749deb3a24c19bf3fc18225f80557561
khalidhub7 <louguikhalid7@gmail.com>
2025-08-08 10:43:53 +0100

    can unlock all ??

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,53 +1,23 @@
 #!/usr/bin/python3
-"""Lockboxes is it locked"""
-
-
-class Keys:
-    """keys class"""
-
-    def __init__(self, numOfBoxes: int):
-        """init function"""
-        self.keys = {}
-        for i in range(numOfBoxes):
-            self.keys[f'{i}'] = True if i == 0 else False
-
-
-class Boxes:
-    """boxes info class"""
-    Locked = []
-
-
-class Box:
-    """box class"""
-
-    def __init__(self, keysList: list, idx: int, k: Keys):
-        """init function"""
-        self._k = k
-        self._keysList = keysList
-        self._idx = idx
-        if str(idx) in k.keys and k.keys[str(idx)] is True:
-            for key in keysList:
-                k.keys[str(key)] = True
-        else:
-            Boxes.Locked.append(self)
-
-    def update(self):
-        """update function"""
-        if str(self._idx) in self._k.keys and\
-                self._k.keys[str(self._idx)] is True:
-            for key in self._keysList:
-                self._k.keys[str(key)] = True
+""" check if all boxes can be unlocked """
+from collections import deque
 
 
 def canUnlockAll(boxes):
-    """canUnlockAll function"""
-    size = len(boxes)
-    k = Keys(size)
-    for i in range(size):
-        Box(boxes[i], i, k)
-    for box in Boxes.Locked:
-        box.update()
-    for val in k.keys.values():
-        if not val:
-            return False
-    return True
+    """ check if all boxes can be unlocked """
+    queue = deque([0])
+    visited = []
+
+    are_all_unlocked = []
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            for i in boxes[node]:
+                if i not in visited:
+                    queue.append(i)
+            are_all_unlocked.append(True)
+
+    # print(are_all_unlocked)
+    # print(visited)
+    return len(are_all_unlocked) == len(boxes)
