4dc97bfafca5f8aaf610d18aabab312381666052
khalidhub7 <louguikhalid7@gmail.com>
2025-08-09 02:33:32 +0100

    cleaning

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,19 +4,25 @@ from collections import deque
 
 
 def canUnlockAll(boxes):
-    """ check if all boxes can be unlocked """
-    queue = deque([0])
+    """
+    boxes: list of lists with keys to other boxes
+    Return True if you can open all boxes, else False
+    """
     visited = []
-
+    queue = deque([0])
     are_all_unlocked = []
+
     while queue:
         node = queue.popleft()
         if node not in visited:
+
             visited.append(node)
             if node < len(boxes):
+
                 for i in boxes[node]:
                     if i not in visited:
                         queue.append(i)
+
                 are_all_unlocked.append(True)
     # print(len(are_all_unlocked))
     # print(len(boxes))
