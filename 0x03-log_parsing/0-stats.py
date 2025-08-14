dbef802f785f009ffae9d5a923f2a029cd3153e1
khalidhub7 <louguikhalid7@gmail.com>
2025-08-14 22:00:08 +0100

    fix regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,8 +6,7 @@ import signal
 from datetime import datetime
 
 
-i = 0
-size = 0
+i = size = 0
 all_status = {
     '200': 0, '301': 0, '400': 0, '401': 0,
     '403': 0, '404': 0, '405': 0, '500': 0
