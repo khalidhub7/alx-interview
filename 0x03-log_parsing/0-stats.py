0f44606768279d8aae71af9bd06067fdf3653e71
khalidhub7 <louguikhalid7@gmail.com>
2025-08-14 14:15:58 +0100

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,7 @@ all_status = {
     '403': 0, '404': 0, '405': 0, '500': 0
 }
 size = 0
-lines = 0
+i = 0
 
 # matches numbers 1–255
 byte_pattern = '(?:[1-9]|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])'
@@ -23,27 +23,6 @@ pattern = (
     r'"GET /projects/260 HTTP/1\.1" (\d+) (\d+)\n$'
 ).format(byte_pattern)
 
-
-def logParsing(line):
-    """ logParsing """
-    global size
-    global lines
-    m = re.match(pattern, line)
-    if m:
-        date = m.group(2)
-        status = m.group(3)
-        if status in all_status:
-            all_status[status] += 1
-        size += int(m.group(4))
-
-        try:
-            valid_date = datetime.strptime(
-                date, "%Y-%m-%d %H:%M:%S.%f")
-        except Exception:
-            valid_date = None
-
-        if valid_date and status in all_status:
-            lines += 1
 
 # define a signal
 
@@ -60,18 +39,23 @@ signal.signal(2, handler)
 
 
 for line in sys.stdin:
-    if lines != 10:
-        logParsing(line)
-    else:
-        print(f'File size: {size}')
-        for k, v in all_status.items():
-            if v != 0:
-                print(f'{k}: {v}')
+    i += 1
+    m = re.match(pattern, line)
+    if m:
+        date = m.group(2)
+        status = m.group(3)
+        if status in all_status:
+            all_status[status] += 1
+        size += int(m.group(4))
 
-        lines = 0
-        """ size = 0
-        all_status = {
-            '200': 0, '301': 0, '400': 0, '401': 0,
-            '403': 0, '404': 0, '405': 0, '500': 0
-        } """
-        logParsing(line)
+        try:
+            valid_date = datetime.strptime(
+                date, "%Y-%m-%d %H:%M:%S.%f")
+        except Exception:
+            valid_date = None
+
+        if i % 10 == 0 and valid_date and status in all_status:
+            print(f'File size: {size}')
+            for k, v in all_status.items():
+                if v != 0:
+                    print(f'{k}: {v}')
